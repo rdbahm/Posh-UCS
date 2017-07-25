f1786e770c805ba68e6b7170eb2726029192a034
Ryan Bahm <rdbahm@users.noreply.github.com>
2017-07-25 13:50:27 -0700

    Automatic reorganize

--- UCS/UcsProvConfig.psm1
+++ UCS/UcsProvConfig.psm1
@@ -4,7 +4,7 @@
 $Script:LocalStorageLocation = "$env:LOCALAPPDATA\UCS"
 $Script:ConfigFileName = 'UcsProvConfig.xml'
 $Script:DisabledSuffix = '-disabled'
-$Script:ConfigPath = Join-Path $Script:LocalStorageLocation $Script:ConfigFileName
+$Script:ConfigPath = Join-Path -Path $Script:LocalStorageLocation -ChildPath $Script:ConfigFileName
 $Script:ImportedConfigInUse = $false
 
 Function New-UcsProvConfigServer
@@ -17,14 +17,39 @@ Function New-UcsProvConfigServer
     [Parameter(Mandatory)][String]$DisplayName
   )
 
-  $ThisNewServer = 1 | Select-Object -Property @{Name='DisplayName';Expression={$DisplayName}},@{Name='ProvServerAddress';Expression={$ProvServerAddress}},@{Name='ProvServerType';Expression={$ProvServerType}},@{Name='Credential';Expression={$Credential}},@{Name='Priority';Expression={$Priority}}
+  $ThisNewServer = 1 | Select-Object -Property @{
+    Name       = 'DisplayName'
+    Expression = {
+      $DisplayName
+    }
+  }, @{
+    Name       = 'ProvServerAddress'
+    Expression = {
+      $ProvServerAddress
+    }
+  }, @{
+    Name       = 'ProvServerType'
+    Expression = {
+      $ProvServerType
+    }
+  }, @{
+    Name       = 'Credential'
+    Expression = {
+      $Credential
+    }
+  }, @{
+    Name       = 'Priority'
+    Expression = {
+      $Priority
+    }
+  }
   
-  Add-UcsProvConfigServer $ThisNewServer
+  Add-UcsProvConfigServer -UcsProvConfigServer $ThisNewServer
 }
 
 Function Get-UcsProvConfig
 {
-  $ThisResult = $Script:ProvConfig | Sort-Object -Property Priority,Index
+  $ThisResult = $Script:ProvConfig | Sort-Object -Property Priority, Index
   
   Return $ThisResult
 }
@@ -39,7 +64,12 @@ Function Add-UcsProvConfigServer
   $ToAddObject = $UcsProvConfigServer
   $Index = [Int](Get-UcsProvConfig | Measure-Object -Property Index -Maximum).Maximum
   $Index++
-  $ToAddObject = $ToAddObject | Select-Object *,@{Name='Index';Expression={$Index}}
+  $ToAddObject = $ToAddObject | Select-Object -Property *, @{
+    Name       = 'Index'
+    Expression = {
+      $Index
+    }
+  }
   
   $null = $Script:ProvConfig.Add($ToAddObject)
   
@@ -57,10 +87,10 @@ Function Import-UcsProvConfigStorage
 
   if((Test-Path $Path) -eq $false)
   {
-    Write-Error "Could not find file at $Path." -ErrorAction Stop
+    Write-Error -Message "Could not find file at $Path." -ErrorAction Stop
   }
 
-  Write-Debug "Importing $Path to UcsConfig."
+  Write-Debug -Message "Importing $Path to UcsConfig."
   $Imported = Import-Clixml -Path $Path
 
   $Script:ProvConfig = $Imported
@@ -77,58 +107,58 @@ Function Update-UcsProvConfigStorage
 
   if( (Test-Path $Directory) -eq $false)
   {
-    Write-Debug "Path $Directory not found. Creating..."
+    Write-Debug -Message "Path $Directory not found. Creating..."
     $null = New-Item -Path $Directory -ItemType Directory -Force
   }
 
   if($Script:ImportedConfigInUse)
   {
-    Write-Debug "Writing XML file to $Path."
+    Write-Debug -Message "Writing XML file to $Path."
     $Script:ProvConfig | Export-Clixml -Path $Path -Depth 1
   }
   else
   {
-    Write-Debug "Imported config not currently in use, not updating."
+    Write-Debug -Message 'Imported config not currently in use, not updating.'
   }
 }
 
 Function Enable-UcsProvConfigStorage
 {
-   [CmdletBinding(SupportsShouldProcess,ConfirmImpact = 'High')]
-   Param()
+  [CmdletBinding(SupportsShouldProcess,ConfirmImpact = 'High')]
+  Param()
 
-   if($Script:ImportedConfigInUse -eq $true)
-   {
-    Write-Error "Config storage already in use."
+  if($Script:ImportedConfigInUse -eq $true)
+  {
+    Write-Error -Message 'Config storage already in use.'
     Break
-   }
+  }
 
-   if($PSCmdlet.ShouldProcess($Script:ConfigPath))
-   {
-     $Script:ImportedConfigInUse = $true
-     Update-UcsConfigStorage
-   }
+  if($PSCmdlet.ShouldProcess($Script:ConfigPath))
+  {
+    $Script:ImportedConfigInUse = $true
+    Update-UcsConfigStorage
+  }
 }
 
 Function Disable-UcsProvConfigStorage
 {
-   [CmdletBinding(SupportsShouldProcess,ConfirmImpact = 'High')]
-   Param()
+  [CmdletBinding(SupportsShouldProcess,ConfirmImpact = 'High')]
+  Param()
 
-   if($Script:ImportedConfigInUse -ne $true)
-   {
-    Write-Error "Config storage not in use."
+  if($Script:ImportedConfigInUse -ne $true)
+  {
+    Write-Error -Message 'Config storage not in use.'
     Break
-   }
+  }
 
-   if($PSCmdlet.ShouldProcess($Script:ConfigPath))
-   {
-     $Script:ImportedConfigInUse = $false
-     $ThisItem = Get-Item -Path $Script:ConfigPath
-     $NewName = ('{0}{1}{2}' -f $ThisItem.BaseName,$Script:DisabledSuffix,$ThisItem.Extension)
-     $NewPath = Join-Path $ThisItem.Directory $NewName
-     $null = Rename-Item -Path $Script:ConfigPath -NewName $NewPath -Force
-   }
+  if($PSCmdlet.ShouldProcess($Script:ConfigPath))
+  {
+    $Script:ImportedConfigInUse = $false
+    $ThisItem = Get-Item -Path $Script:ConfigPath
+    $NewName = ('{0}{1}{2}' -f $ThisItem.BaseName, $Script:DisabledSuffix, $ThisItem.Extension)
+    $NewPath = Join-Path -Path $ThisItem.Directory -ChildPath $NewName
+    $null = Rename-Item -Path $Script:ConfigPath -NewName $NewPath -Force
+  }
 }
 
 Function Get-UcsProvConfigStorageIsEnabled
@@ -139,11 +169,11 @@ Function Get-UcsProvConfigStorageIsEnabled
 
 <### INITIALIZATION ###>
 
-$Script:ProvConfig = New-Object System.Collections.ArrayList
+$Script:ProvConfig = New-Object -TypeName System.Collections.ArrayList
 
 <#### Check for preferences ####>
 if( Test-Path $Script:ConfigPath )
 {
-  Write-Debug "Found config file at $Script:ConfigPath."
+  Write-Debug -Message "Found config file at $Script:ConfigPath."
   Import-UcsProvConfigStorage
 }
